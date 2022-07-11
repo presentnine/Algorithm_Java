8e2356db1b7e4dd177121e83a0e22df336bd1e73
Hyun Ku Kim <presentnine@naver.com>
2022-07-11 23:15:25 +0900

    add solution

--- src/com/algorithm/LeetCode_Climbing_Stairs.java
+++ src/com/algorithm/LeetCode_Climbing_Stairs.java
@@ -3,3 +3,22 @@ package com.algorithm;
 //https://leetcode.com/problems/climbing-stairs/
 public class LeetCode_Climbing_Stairs {
 }
+class Solution_LeetCode_Climbing_Stairs {
+    public int climbStairs(int n) {
+        if (n == 1) {//계단 한 개
+            return 1;
+        } else if (n == 2) {//계단 두 개
+            return 2;
+        }
+
+        int[] acc = new int[n];//누적값 배열
+        acc[0] = 1;
+        acc[1] = 2;
+
+        for (int i = 2; i < n; i++) {//점화식
+            acc[i] = acc[i - 1] + acc[i - 2];
+        }
+
+        return acc[n - 1];
+    }
+}
--- src/com/algorithm/LeetCode_Jump_Game.java
+++ src/com/algorithm/LeetCode_Jump_Game.java
@@ -2,4 +2,25 @@ package com.algorithm;
 
 //https://leetcode.com/problems/jump-game/
 public class LeetCode_Jump_Game {
+
+}
+
+class Solution_LeetCode_Jump_Game {
+    public boolean canJump(int[] nums) {
+        boolean[] possible = new boolean[nums.length];//가능성 체크 배열
+        possible[nums.length - 1] = true;//마지막은 true
+
+        for (int i = nums.length - 2; i >= 0; i--) {//끝에서부터 앞으로
+            int stepCount = nums[i];
+
+            for (int j = i + 1; j <= i + stepCount && j < nums.length; j++) {//주어진 횟수만큼
+                if (possible[j]) {//앞에 true가 있다면 현재도 true로 변경
+                    possible[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return possible[0];//시작부분 반환
+    }
 }
