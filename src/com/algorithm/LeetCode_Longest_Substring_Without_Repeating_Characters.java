38c540e46afe90efa9cf8aa0ae5590ffc1a46459
Hyun Ku Kim <presentnine@naver.com>
2023-01-12 01:11:41 +0900

    Update LeetCode_Longest_Substring_Without_Repeating_Characters.java

--- src/com/algorithm/LeetCode_Longest_Substring_Without_Repeating_Characters.java
+++ src/com/algorithm/LeetCode_Longest_Substring_Without_Repeating_Characters.java
@@ -1,5 +1,6 @@
 package com.algorithm;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -9,32 +10,29 @@ public class LeetCode_Longest_Substring_Without_Repeating_Characters {
 
 class Solution_LeetCode_Longest_Substring_Without_Repeating_Characters {
     public int lengthOfLongestSubstring(String s) {
-        HashSet<Character> hashSet = new HashSet<>();
-        Queue<Character> q = new LinkedList<>();
+        if (s.length() <= 1) {
+            return s.length();
+        }
+
         int answer = 0;
+        HashSet<Character> hashSet = new HashSet<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            Character c = s.charAt(i);
+        int start = 0, end;
+        hashSet.add(s.charAt(start));
 
-            if (hashSet.contains(c)) {
-                StringBuilder sb = new StringBuilder();
-                while (!q.isEmpty()) {
-                    sb.append(q.poll());
-                }
+        for (end = 1; end < s.length(); end++) {
+            if (hashSet.contains(s.charAt(end))) {//마지막과 겹치는 문자가 있다면
+                answer = Math.max(answer, end - start);//현재까지의 문자열 비교
 
-                answer = Math.max(answer, sb.length());
-                hashSet = new HashSet<>();
+                do {//겹치는 문자가 없어질 때까지 start를 앞으로 땡김
+                    hashSet.remove(s.charAt(start));
+                } while (s.charAt(start++) != s.charAt(end));
             }
-            hashSet.add(c);
-            q.add(c);
+
+            hashSet.add(s.charAt(end));//마지막 부분 추가
         }
 
-        StringBuilder sb = new StringBuilder();
-        while (!q.isEmpty()) {
-            sb.append(q.poll());
-        }
-
-        answer = Math.max(answer, sb.length());
+        answer = Math.max(answer, end - start);//최종 문자열도 비교
 
         return answer;
     }
