7cba765228b3a48a1b5e240738991e1809ad62db
Hyun Ku Kim <presentnine@naver.com>
2023-01-18 11:56:06 +0900

    Create P1003.java

---
+++ src/com/algorithm/P1003.java
@@ -0,0 +1,29 @@
+package com.algorithm;
+
+//https://www.acmicpc.net/problem/1003
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1003 {
+    static int[][] dp = new int[41][2];
+
+    public static void main(String[] args) throws IOException {
+        dp[0] = new int[]{1, 0};
+        dp[1] = new int[]{0, 1};
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i < 41; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+        }
+
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            System.out.println(dp[N][0] + " " + dp[N][1]);
+        }
+    }
+}
