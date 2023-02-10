9465a91e2b80c0c6c19c74902cc32298f5c987be
Hyun Ku Kim <presentnine@naver.com>
2023-02-10 18:56:01 +0900

    Update Baekjoon_15486.java

--- src/com/algorithm/Baekjoon_15486.java
+++ src/com/algorithm/Baekjoon_15486.java
@@ -5,27 +5,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 //https://www.acmicpc.net/problem/15486
+
 public class Baekjoon_15486 {
-    static int[][] schedule;
     static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1];
 
-        schedule = new int[N][2];
-        dp = new int[N];
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             String[] tAndP = br.readLine().split(" ");
-            schedule[i][0] = Integer.parseInt(tAndP[0]);
-            schedule[i][1] = Integer.parseInt(tAndP[1]);
-        }
+            int time = Integer.parseInt(tAndP[0]);
+            int price = Integer.parseInt(tAndP[1]);
 
-        for (int i = 0; i < N; i++) {
-            if (schedule[i][0] + i - 1 < N) {
-                dp[schedule[i][0] + i - 1] += schedule[i][1];
+            dp[i] = Math.max(dp[i], dp[i - 1]);
+            if (i + time - 1 <= N) {
+                dp[i + time - 1] = Math.max(dp[i + time - 1], dp[i - 1] + price);
             }
         }
+
+        System.out.println(dp[N]);
     }
 }
