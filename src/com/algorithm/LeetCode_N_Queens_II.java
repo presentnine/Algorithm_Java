54088c4a4a5e9bb8156402d9aaebc1d410036087
Hyun Ku Kim <presentnine@naver.com>
2023-03-09 21:25:50 +0900

    Create LeetCode_N_Queens_II.java

---
+++ src/com/algorithm/LeetCode_N_Queens_II.java
@@ -0,0 +1,75 @@
+package com.algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//https://leetcode.com/problems/n-queens-ii/
+
+public class LeetCode_N_Queens_II {
+}
+
+class Solution_LeetCode_N_Queens_II {
+    char[][] chessboard;
+    boolean[] usedCol;
+    int answer = 0;
+
+    public int totalNQueens(int n) {
+        chessboard = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(chessboard[i], '.');
+        }
+        usedCol = new boolean[n];
+
+        simulation(0, n);
+        return answer;
+    }
+
+    void simulation(int nextRow, int n) {
+        if (nextRow == n) {//모든 퀸 배정 완료
+            if (check(n)) {//유니크한 상태라면 답에 추가
+                ++answer;
+            }
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {//해당 행에 속한 열을 모두 돌며
+            if (!usedCol[i]) {//퀸을 놓을 수 있는 자리면 퀸을 놓고 다음 행 탐색(백트래킹)
+                usedCol[i] = true;
+                chessboard[nextRow][i] = 'Q';
+                simulation(nextRow + 1, n);
+                chessboard[nextRow][i] = '.';
+                usedCol[i] = false;
+            }
+        }
+    }
+
+    boolean check(int n) {
+        for (int i = 0; i < n - 1; i++) {//마지막을 제외한 전체 행을 돌며
+            for (int j = 0; j < n; j++) {
+                if (chessboard[i][j] == 'Q') {//퀸 위치면
+                    int step = 1;
+
+                    while (i + step < n) {//해당 퀸부터 대각선으로 내려오는 방향의 위치들에 대해
+                        if (j - step >= 0) {//왼쪽 아래 방향
+                            if (chessboard[i + step][j - step] == 'Q') {//퀸이 중복
+                                return false;
+                            }
+                        }
+
+                        if (j + step < n) {//오른쪽 아래 방향
+                            if (chessboard[i + step][j + step] == 'Q') {//퀸이 중복
+                                return false;
+                            }
+                        }
+                        ++step;
+                    }
+
+                    continue;//해당 행 건너뛰기
+                }
+            }
+        }
+
+        return true;
+    }
+}
